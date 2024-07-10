ecb8fc10b971e3dfc83b74b493c57b8c532a1349
mayu19991226 <mayu19991226@icloud.com>
2024-07-10 21:47:36 +0900

    デプロイの設定

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,11 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-bundle exec rails db:migrate RAILS_ENV=development
-bundle exec rails db:seed
+# Run migrations
+bundle exec rails db:migrate RAILS_ENV=production
+
+# Seed the database (optional)
+bundle exec rails db:seed RAILS_ENV=production
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
