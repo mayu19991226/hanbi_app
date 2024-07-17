15a54c2acf295bc62c3bf70a61dd09f0c5345add
mayu19991226 <mayu19991226@icloud.com>
2024-07-17 22:53:33 +0900

    ハンバーガーメニュー完了

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    'node_modules/preline/dist/*.js'
   ],
   theme: {
     extend: {
-      // フォントファミリーを追記
       fontFamily: {
         ttl: ['Zen Kaku Gothic Antique', 'sans-serif'],
         main: [
@@ -18,15 +18,21 @@ module.exports = {
           'MS Mincho', 'MS 明朝'
         ]
       },
-      // カラーを追記
       colors: {
         theme: {
           main: '#008742',
           font: '#333333',
           bg: '#FCF5E9',
-        }
-      }
-    }
+        },
+        // ここに透過度の設定を追加
+        backgroundColor: {
+          'white-25': 'rgba(255, 255, 255, 0.25)', // 25%の透過度の白色
+          'bg-opacity-25': 'rgba(252, 245, 233, 0.25)', // 25%の透過度の特定の背景色
+        },
+      },
+    },
   },
-  plugins: []
-}
+  plugins: [
+    require('preline/plugin')
+  ],
+};
