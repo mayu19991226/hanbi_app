3d5e04f9c4f70e561d3e210b47ee07398511ef75
mayu19991226 <mayu19991226@icloud.com>
2024-07-19 22:52:51 +0900

    ActiveRecord::PendingMigrationError解消のため2

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ rm -f /myapp/tmp/pids/server.pid
 bundle exec rails db:migrate RAILS_ENV=production
 
 # Seed the database (optional)
-# bundle exec rails db:seed RAILS_ENV=production
+bundle exec rails db:seed RAILS_ENV=production
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
