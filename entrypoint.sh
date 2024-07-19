fbace01ac5c57a3729be76234afbeaf3390aba93
mayu19991226 <mayu19991226@icloud.com>
2024-07-19 21:56:00 +0900

    DB再作成

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,10 @@ set -e
 rm -f /myapp/tmp/pids/server.pid
 
 # Run migrations
-bundle exec rails db:migrate RAILS_ENV=production
+# bundle exec rails db:migrate RAILS_ENV=production
 
 # Seed the database (optional)
-bundle exec rails db:seed RAILS_ENV=production
+# bundle exec rails db:seed RAILS_ENV=production
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
