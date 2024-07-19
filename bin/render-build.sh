5cad58e9ae505ebcbc4a0e93b9ecbf910693f640
mayu19991226 <mayu19991226@icloud.com>
2024-07-19 12:52:55 +0900

    ビルドスクリプトの変更を元に戻す

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,9 +8,5 @@ yarn build # jsファイルをesbuildでバンドルしているため
 yarn build:css
 bundle exec rake assets:precompile # cssはsprocketsを使っているため
 bundle exec rake assets:clean
-# bundle exec rake db:migrate 一旦コメントアウト
+bundle exec rake db:migrate
 # bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile # migrateはridgepoleを使っているため（標準のmigrateを使うならbundle exec rails db:migrateで良いかと思います）
-
-# 以下の2行を追加
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
-bundle exec rake db:seed
