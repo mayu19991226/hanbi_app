d1f069e055ba40d7ba72f2343ed482056d21f436
mayu19991226 <mayu19991226@icloud.com>
2024-07-24 22:58:02 +0900

    本番環境でパスワード再設定が上手くいくか確認

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,4 +92,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Action Mailerの設定
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'hanbi-app.onrender.com',  # ドメイン名
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { host: 'hanbi-app.onrender.com' }  # Renderのドメイン名
 end
