8ca8569298deb68ca6052060bc6ab6bada4b48ab
mayu19991226 <mayu19991226@icloud.com>
2024-07-25 15:11:30 +0900

    模索中

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.perform_deliveries = true
 
+  # 開発環境のURL
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # 本番のURLにしてみる
   # config.action_mailer.default_url_options = { host: 'hanbi-app.onrender.com', protocol: 'https' }
