6e825747e4faacd0bd9d7d2220e04b37397124ab
mayu19991226 <mayu19991226@icloud.com>
2024-09-12 17:40:19 +0900

    コラムのサイドバーに関連付いた投稿を表示

--- app/controllers/area_categories_controller.rb
+++ app/controllers/area_categories_controller.rb
@@ -4,5 +4,6 @@ class AreaCategoriesController < ApplicationController
   def show
     @area_category = AreaCategory.find(params[:id])
     @area_categories = AreaCategory.all
+    @posts = @area_category.posts.order(created_at: :desc).limit(6) # 最新の投稿を6件取得
   end
 end
